3481fd9358092c7da0a7136afa78200163a99c7c
Davi Roberto <daviroberto11@hotmail.com>
2020-08-08 16:25:07 -0300

    routes config

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "mt-about",
+  templateUrl: "./about.component.html",
+})
+export class AboutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 
+import { ROUTES } from "./app.routes";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    RouterModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent],
+  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
