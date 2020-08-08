fd8b24c64ecca7beafeaea42e5cafbcc844537d8
Davi Roberto <daviroberto11@hotmail.com>
2020-08-08 19:10:59 -0300

    classe de serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,21 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
+import { RestaurantsService } from "./restaurants/restaurants.service";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+  ],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
-  providers: [],
+  providers: [RestaurantsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
