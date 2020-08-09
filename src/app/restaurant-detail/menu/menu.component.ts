804328bf952978f3cacc473350b56f536ca42eee
Davi Roberto <daviroberto11@hotmail.com>
2020-08-09 10:59:46 -0300

    serviço detalhe do restaurante

---
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "mt-menu",
+  templateUrl: "./menu.component.html",
+})
+export class MenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { RestaurantsService } from "../restaurants/restaurants.service";
+
+import { Restaurant } from "../restaurants/restaurant/restaurant.model";
+
+@Component({
+  selector: "mt-restaurant-detail",
+  templateUrl: "./restaurant-detail.component.html",
+})
+export class RestaurantDetailComponent implements OnInit {
+  restaurant: Restaurant;
+
+  constructor(
+    private restaurantService: RestaurantsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.restaurantService
+      .restaurantById(this.route.snapshot.params["id"])
+      .subscribe((restaurant) => (this.restaurant = restaurant));
+  }
+}
